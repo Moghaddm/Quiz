42dc2a5577bad1c6e0ae36e2bf8e571bfd1d96a9
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2023-12-06 22:19:17 +0330

    Init Question Entity Model

---
+++ src/NovinQuiz.Domain/Modules/Questions/Services/QuestionPropertiesManagerService.cs
@@ -0,0 +1,46 @@
+﻿using NovinQuiz.Modules.Quizzes.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Services;
+
+namespace NovinQuiz.Modules.Questions.Services
+{
+    public class QuestionPropertiesManagerService : DomainService
+    {
+        public List<Question> CheckSumScore(List<Question> questions, byte passScore)
+        {
+            int sumScore = questions.Sum(q => q.Score);
+
+            if (sumScore > passScore)
+                throw new PassScoreCannotBeMoreThanSumQuestionScoresException("Domain:PassScoreCannotBeMoreThanSumQuestionScores")
+                {
+                    Data =
+                    {
+                        { "SumScores", sumScore }
+                    }
+                };
+
+            return questions;
+        }
+
+        public Question CheckSumScore(Question question, byte passScore, List<Question> questions)
+        {
+
+            int sumScore = questions.Sum(q => q.Score);
+
+            if (sumScore + question.Score > passScore)
+                throw new PassScoreCannotBeMoreThanSumQuestionScoresException("Domain:PassScoreCannotBeMoreThanSumQuestionScores")
+                {
+                    Data =
+                    {
+                        { "SumScores", sumScore }
+                    }
+                };
+
+            return question;
+        }
+    }
+}
