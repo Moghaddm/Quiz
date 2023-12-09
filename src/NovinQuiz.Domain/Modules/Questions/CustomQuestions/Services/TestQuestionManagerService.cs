34d101fbed10dc2bd1d5fe608939dd7784fc9deb
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2023-12-09 20:12:50 +0330

    Update Custom Question Services

---
+++ src/NovinQuiz.Domain/Modules/Questions/CustomQuestions/Services/TestQuestionManagerService.cs
@@ -0,0 +1,24 @@
+﻿using NovinQuiz.Modules.Questions.CustomQuestions.Exceptions;
+using NovinQuiz.Modules.Questions.CustomQuestions.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace NovinQuiz.Modules.Questions.CustomQuestions.Services
+{
+    public class TestQuestionManagerService
+    {
+        public List<TestQuestionChoice> CheckQuestionChoicesValid(List<TestQuestionChoice> choices)
+        {
+            Check.NotNull(choices, nameof(choices));
+
+            if (choices.Count(c => c.IsCorrect) > 1)
+                throw new CorrectAnswerCannotBeMoreThanOneException("Domain:CorrectAnswerCannotBeMoreThanOne");
+
+            return choices;
+        }
+    }
+}
