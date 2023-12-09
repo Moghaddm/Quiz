2570f169c47a3e6c07e5bd904071c82ed8b03ae3
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2023-12-09 20:13:22 +0330

    Update Custom Question Value Objects

---
+++ src/NovinQuiz.Domain/Modules/Questions/CustomQuestions/ValueObjects/ResponseMethodsDescriptiveQuestions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovinQuiz.Modules.Questions.CustomQuestions.ValueObjects
+{
+    public record struct ResponseMethodsDescriptiveQuestions(bool CanInsertText, bool CanRecordAudio, bool CanSendFile, bool CanSendPhoto, bool CanSendVideo);
+}
