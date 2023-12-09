97f1ea5a2431f95e60402c4b7db7e9ebb1570a83
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2023-12-09 14:24:40 +0330

    Update Quiz Models

---
+++ src/NovinQuiz.Domain/Modules/Quizzes/Services/DescriptiveScoreManagerService.cs
@@ -0,0 +1,48 @@
+﻿using NovinQuiz.Modules.Quizzes.Exceptions;
+using NovinQuiz.Modules.Quizzes.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace NovinQuiz.Modules.Quizzes.Services
+{
+    public class DescriptiveScoreManagerService : DomainService
+    {
+        public List<DescriptiveScore> CheckDescriptiveScoresValid(List<DescriptiveScore> descriptiveScores)
+        {
+            Check.NotNull(descriptiveScores, nameof(descriptiveScores));
+
+            if (descriptiveScores.Count > 6)
+                throw new DescriptiveScoresCountMoreThanSixException("Domain:DescriptiveScoresCountMoreThanSix")
+                    .WithData("DescriptiveScore", nameof(descriptiveScores));
+
+            for (int i = 1; i <= descriptiveScores.Count - 1; i++)
+            {
+                if (descriptiveScores[i].ScorePeriod == descriptiveScores[i - 1].ScorePeriod)
+                    throw new ScorePeriodCannotBeRepeatAsPerviousDescriptiveScoreException("Domain:ScorePeriodCannotBeRepeatAsPerviousDescriptiveScore")
+                    {
+                        Data =
+                        {
+                            {"ScorePeriod", descriptiveScores[i].ScorePeriod}
+                        }
+                    };
+
+                else if (descriptiveScores[i].ScorePeriod < descriptiveScores[i - 1].ScorePeriod)
+                    throw new ScorePeriodCannotBeMoreThanPerviousDescriptiveScoreException("Domain:ScorePeriodCannotBeMoreThanPerviousDescriptiveScore")
+                    {
+                        Data =
+                        {
+                            {"ScorePeriod", descriptiveScores[i].ScorePeriod},
+                            {"PerviousScorePeriod", descriptiveScores[i - 1].ScorePeriod}
+                        }
+                    };
+            }
+
+            return descriptiveScores!;
+        }
+    }
+}
--- src/NovinQuiz.Domain/Modules/Quizzes/ValueObjects/MultipleParticipateSettings.cs
+++ src/NovinQuiz.Domain/Modules/Quizzes/ValueObjects/MultipleParticipateSettings.cs
@@ -4,21 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Domain.Values;
 
 namespace NovinQuiz.Modules.Quizzes.ValueObjects
 {
-    public class MultipleParticipateSettings : ValueObject
+    public record MultipleParticipateSettings(byte multipleParticipateChanceCount, MultipleParticipateScoreStatus multipleParticipateScoreStatus)
     {
         public byte MultipleParticipateChanceCount { get; private set; }
+            = (byte)Check.Range(multipleParticipateChanceCount, nameof(multipleParticipateChanceCount), (short)1, (short)10);
+
         public MultipleParticipateScoreStatus MultipleParticipateScoreStatus { get; private set; }
+            = Check.NotNull(multipleParticipateScoreStatus, nameof(multipleParticipateScoreStatus));
 
-        public MultipleParticipateSettings(byte MultipleParticipateChanceCount, MultipleParticipateScoreStatus MultipleParticipateScoreStatus)
-        {
-
-        }
-
-        protected override IEnumerable<object> GetAtomicValues()
+        public IEnumerable<object> GetAtomicValues()
         {
             yield return MultipleParticipateChanceCount;
             yield return MultipleParticipateScoreStatus;
