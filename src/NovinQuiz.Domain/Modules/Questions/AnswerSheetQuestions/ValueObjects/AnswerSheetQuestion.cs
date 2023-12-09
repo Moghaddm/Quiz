e6ba06fd68773f70c7427a37ff66eb3b6da61a1d
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2023-12-09 14:22:59 +0330

    Update Answer Sheet Questions Model

---
+++ src/NovinQuiz.Domain/Modules/Questions/AnswerSheetQuestions/ValueObjects/AnswerSheetQuestion.cs
@@ -0,0 +1,17 @@
+﻿using NovinQuiz.Modules.Questions.AnswerSheetQuestions.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace NovinQuiz.Modules.Questions.AnswerSheetQuestions.ValueObjects
+{
+    public record AnswerSheetQuestion(byte questionNumber, AnswerSheetQuestionChoices choices, byte correctChoice)
+    {
+        public short QuestionNumber { get; private set; } = Check.Positive(questionNumber, nameof(questionNumber));
+        public AnswerSheetQuestionChoices Choices { get; private set; } = choices;
+        public byte CorrectChoice { get; private set; } = (byte)Check.Range(correctChoice, nameof(correctChoice), 1, (int)choices);
+    }
+}
---
+++ src/NovinQuiz.Domain/Modules/Questions/AnswerSheetQuestions/ValueObjects/CustomQuestionAttachments.cs
@@ -0,0 +1,20 @@
+﻿using NovinQuiz.Modules.Common.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Values;
+
+namespace NovinQuiz.Modules.Questions.AnswerSheetQuestions.ValueObjects
+{
+    public record CustomQuestionAttachments(Attachment? photo, Attachment? recordAudio)
+    {
+        public Attachment Photo { get; private set; } = photo;
+        public void SetPhoto(Attachment? photo) => Photo = photo;
+
+        public Attachment RecordAudio { get; private set; } = recordAudio;
+        public void SetRecordAudio(Attachment? recordAudio) => RecordAudio = recordAudio;
+    }
+}
