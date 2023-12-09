b502272833bd3679a0e8706d07e710f3effce008
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2023-12-09 14:23:17 +0330

    Update Common Models

---
+++ src/NovinQuiz.Domain/Modules/Common/ValueObjects/Attachment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Values;
+
+namespace NovinQuiz.Modules.Common.ValueObjects
+{
+    public class Attachment : Entity<string>
+    {
+        public string Address { get; private set; }
+        public string FileName { get; private set; }
+        public string Alt { get; private set; }
+
+        public Attachment(string address) => Address = Check.NotNullOrEmpty(address, nameof(address));
+
+        public static implicit operator string(Attachment instance) => instance.Address;
+    }
+}
